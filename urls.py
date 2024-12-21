98c25a775212cad521447954d3aea617fcf8e5c0
soul <155264295+soulgin@users.noreply.github.com>
2024-12-21 20:07:44 +0200

    Settings - urls - wsgo
    For main project not app

--- urls.py
+++ urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from tasks import views
 
 urlpatterns = [
-    path('', views.view_month, name='view_month'),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'), 
+    path('month/<int:year>/<int:month>/', views.month_view, name='month_view'),
     path('add_task/', views.add_task, name='add_task'),
+    path('tasks/', include('tasks.urls')),
+     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
